0aa9b8064b7b4467fed5cc1aff753c1a29b3644d
Darío Candotti <dario.candotti@gmail.com>
2025-07-27 14:35:53 -0300

    Add: phpunit with a first smoking test.

---
+++ tests/smoke/EngineBootsTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XO\Tests\Smoke;
+
+use PHPUnit\Framework\TestCase;
+
+final class EngineBootsTest extends TestCase
+{
+	public function test_engine_is_alive(): void
+	{
+		$this->assertTrue(true);
+	}
+}
