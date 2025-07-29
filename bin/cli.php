b52f52948c155d1834c04b40363758a9fd0dd339
Darío Candotti <dario.candotti@gmail.com>
2025-07-29 19:38:27 -0300

    Add: main function for console execution.

---
+++ bin/cli.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+declare(strict_types=1);
+
+require $_composer_autoload_path ?? dirname(__DIR__) . '/vendor/autoload.php';
+
+use XO\ConsoleInput;
+use XO\ConsoleOutput;
+use XO\HumanPlayer;
+use XO\AIPlayer;
+use XO\ClassicGame;
+
+$input = new ConsoleInput();
+$output = new ConsoleOutput();
+$playerOne = new HumanPlayer($input);
+$playerTwo = new AIPlayer();
+(new ClassicGame($playerOne, $playerTwo))->play($input, $output);
